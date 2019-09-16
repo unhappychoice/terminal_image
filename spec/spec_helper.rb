b84a115298a1f63469bc2b50935dbac725d96e3a
Yuji Ueki <unhappychoice@gmail.com>
2019-09-17 00:36:30 +0900

    :green_heart: Send coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,16 @@
 # frozen_string_literal: true
 
+if ENV['COVERAGE']
+  require 'simplecov'
+  SimpleCov.add_filter ['spec']
+  SimpleCov.start
+end
+
+if ENV['CODECOV_TOKEN']
+  require 'codecov'
+  SimpleCov.formatter = SimpleCov::Formatter::Codecov
+end
+
 require 'bundler/setup'
 require 'terminal_image'
 
