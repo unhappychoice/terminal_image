8f5c6926ea2fd5a8523c439f8c012f21561738fd
Yuji Ueki <unhappychoice@gmail.com>
2019-09-16 15:13:29 +0900

    :+1: Add method to show images from url

--- lib/terminal_image.rb
+++ lib/terminal_image.rb
@@ -1,31 +1,43 @@
 require "terminal_image/version"
 require 'fastimage'
+require 'open-uri'
+require 'tempfile'
 
 module TerminalImage
   class UnsupportedTerminal < StandardError; end
 
   class << self
+    def show_url(url)
+      tempfile = Tempfile.create
+      tempfile.binmode
+      URI.open(url) { |o| tempfile.write o.read }
+      TerminalImage.show(tempfile)
+    end
+
     def show(file)
+      puts encode(file)
+    end
+
+    def encode(file)
       if ENV['TERM_PROGRAM'] == 'iTerm.app'
-        show_on_iterm2(file)
+        encode_for_iterm2(file)
       elsif which 'img2sixel'
-        show_by_libsixel(file)
+        encode_for_libsixel(file)
       else
+        puts 'Use iTerm2 or install libsixel according to https://github.com/saitoha/libsixel#install'
         raise UnsupportedTerminal, 'Unsupported terminal'
       end
     end
 
     private
 
-    def show_by_libsixel(file)
-      print `img2sixel < #{file.path} 2>/dev/null`
+    def encode_for_iterm2(file)
+      width, height = FastImage.size(file)
+      "\033]1337;File=inline=1;width=#{width}px;height=#{height}px:#{Base64.strict_encode64(file.read)}\a\n"
     end
 
-    def show_on_iterm2(file)
-      width, height = FastImage.size(file)
-      print "\033]1337;File=inline=1;width=#{width}px;height=#{height}px:"
-      print Base64.strict_encode64(file.read)
-      print "\a\n"
+    def encode_for_libsixel(file)
+      `img2sixel < #{file.path} 2>/dev/null`
     end
 
     def which(cmd)
